960c216477e7b7d5700756bc7001c79d9eb6573f
ndaru <80800278+sbh-camunda@users.noreply.github.com>
2021-06-20 21:12:51 +0700

    add cors

--- src/main/java/com/sbh/bpm/Application.java
+++ src/main/java/com/sbh/bpm/Application.java
@@ -1,8 +1,14 @@
 package com.sbh.bpm;
 
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @EnableProcessApplication
 @SpringBootApplication
@@ -10,6 +16,26 @@ public class Application {
 
   public static void main(String... args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Value("${cors.urls}")
+  String cors_urls;
+
+  @Bean
+  public FilterRegistrationBean processCorsFilter() {
+      UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+      CorsConfiguration config = new CorsConfiguration();
+      // config.setAllowCredentials(true);
+      for(String url: cors_urls.split(",")) {
+        config.addAllowedOrigin(url);
+      }
+      config.addAllowedHeader("*");
+      config.addAllowedMethod("*");
+      source.registerCorsConfiguration("/**", config);
+
+      FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+      bean.setOrder(0);
+      return bean;
   }
 
 }
