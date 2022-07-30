a9bb774a91ea90493762774064d4a5e3d4879571
ndaru <80800278+sbh-camunda@users.noreply.github.com>
2022-07-31 00:16:56 +0700

    async download

--- src/main/java/com/sbh/bpm/service/IZipService.java
+++ src/main/java/com/sbh/bpm/service/IZipService.java
@@ -2,8 +2,12 @@ package com.sbh.bpm.service;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.CompletableFuture;
 import java.util.zip.ZipOutputStream;
+
+import org.camunda.bpm.engine.task.Task;
 
 public interface IZipService {
   void zipFile(File fileToZip, String fileName, ZipOutputStream zipOut) throws IOException;
+  CompletableFuture<Boolean> CreateProjectAttachmentArchived(Task task, Integer certificationTypeId, String projectType) throws Exception;
 }
